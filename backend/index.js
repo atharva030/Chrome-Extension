a0d1be866819717b83ec5d0a61f961b057407f69
atharva030 <atharvas_jagdale@auton.in>
2023-01-21 13:13:30 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 var cors=require('cors')
 const mongoose=require('mongoose');
-require('dotenv').config({ path: './env/pass.env' });
+require('dotenv').config({ path: './pass.env' });
 const mongoURI=process.env.mongoURI
 const app = express()
 const port = 5000 
